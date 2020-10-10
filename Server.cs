df98f4a456ecf0314207a7c451113e3398261213
nguyen16112000 <72647141+nguyen16112000@users.noreply.github.com>
2020-10-10 15:20:29 +0700

    Add files via upload

---
+++ Server.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+using System.Net;
+using System.Collections.Generic;
+namespace PBL4HDHM
+{
+    class Server
+    {
+        static readonly object _lock = new object();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            try
+            {
+                IPAddress IP = IPAddress.Parse("192.168.1.2");
+                TcpListener ServerSocket = new TcpListener(IP, 9000);
+                ServerSocket.Start();
+
+                while (true)
+                {
+                    TcpClient client = ServerSocket.AcceptTcpClient();
+                    lock (_lock) list_clients.Add(count, client);
+                    Console.WriteLine("Co ket noi!");
+
+                    Thread t = new Thread(Handle_clients);
+                    t.Start(count);
+                    count++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void Handle_clients(object o)
+        {
+            int id = (int)o;
+            TcpClient client;
+
+            lock (_lock) client = list_clients[id];
+
+            try
+            {
+                while (true)
+                {
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                    if (byte_count == 0)
+                    {
+                        break;
+                    }
+
+                    string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                    Broadcast(data, client);
+                    Console.WriteLine(data);
+                }
+
+                lock (_lock) list_clients.Remove(id);
+                client.Client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static string Handle(string data)
+        {
+
+            return data;
+        }
+
+        public static void Broadcast(string data, TcpClient sender)
+        {
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(Handle(data) + Environment.NewLine);
+
+                lock (_lock)
+                {
+                    NetworkStream stream = sender.GetStream();
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
